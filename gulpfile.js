6ad965b5c58811b34deb75a42220beff32134afe
Stepan <fenixrnd@mail.ru>
2018-10-13 00:18:04 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,39 @@
 var gulp = require('gulp'),
-browserSync = require('browser-sync').create(),
-reload = browserSync.reload,
-cleanCSS = require('gulp-clean-css'),
-rename = require('gulp-rename'),
-autoprefixer = require('gulp-autoprefixer');
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload,
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('css', function(){
-return gulp.src("app/css/*.css")
-.pipe(rename({suffix: '.min', prefix: ''}))
-.pipe(autoprefixer({
-browsers: ['last 10 versions'],
-cascade: false
-}))
-.pipe(cleanCSS())
-.pipe(gulp.dest('app/dist'))
-.pipe(browserSync.stream())
-.pipe(browserSync.reload({stream: true}));
+gulp.task('css', function() {
+    return gulp.src("app/css/*.css")
+        .pipe(rename({
+            suffix: '.min',
+            prefix: ''
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 10 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('app/dist'))
+        .pipe(browserSync.stream())
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('browser-sync', function(){
-browserSync.init({
-server: {
-baseDir: "./app"
-}
-});
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./app"
+        }
+    });
 });
 ///================================
-gulp.task('watch', ['browser-sync', 'css'], function(){
-gulp.watch("app/css/*.css", ['css']);
-gulp.watch("app/js/*.js").on("change", reload);
-gulp.watch("app/*.html").on("change", reload);
+gulp.task('watch', ['browser-sync', 'css'], function() {
+    gulp.watch("app/css/*.css", ['css']);
+    gulp.watch("app/js/*.js").on("change", reload);
+    gulp.watch("app/*.html").on("change", reload);
 });
 gulp.task('default', ['watch']);
